4163e89ccf5a565b9662ab2a237602d27890c256
Mike Shields <mshields@psi-it.com>
2019-02-11 11:23:12 -0500

    fix bug in config.ts

--- cmd/setup/frontEndFiles/frontEndTemplates/src/config.ts
+++ cmd/setup/frontEndFiles/frontEndTemplates/src/config.ts
@@ -3,7 +3,7 @@ interface URLConfig {
     siteUrl: string;
 }
 let UrlConfig: URLConfig;
-if (window.location.host == "localhost"){
+if (window.location.host.startsWith("localhost")){
     UrlConfig = { 
         apiUrl: "http://localhost:3000", 
         siteUrl: "http://localhost:8080" 
