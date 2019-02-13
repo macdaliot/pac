f32332a47d47a8cbb95ca0b13cebacdb38fe69fd
Jeff Diederiks <jdiederiks@psi-it.com>
2019-02-13 14:14:57 -0500

    'pac clean-cloud' now supports deletion of Route53 resources and disabling of Cloudfront distributions

--- cmd/cleanCloud/deleteAllResources.go
+++ cmd/cleanCloud/deleteAllResources.go
@@ -2,7 +2,9 @@ package cleanCloud
 
 import (
   "github.com/PyramidSystemsInc/go/aws"
+  "github.com/PyramidSystemsInc/go/aws/cloudfront"
   "github.com/PyramidSystemsInc/go/aws/resourcegroups"
+  "github.com/PyramidSystemsInc/go/aws/route53"
   "github.com/PyramidSystemsInc/go/logger"
   "github.com/PyramidSystemsInc/go/str"
 )
@@ -12,6 +14,17 @@ func DeleteAllResources(projectName string) {
   awsSession := aws.CreateAwsSession(region)
   resourcegroups.Create(projectName, "pac-project-name", projectName, awsSession)
   resourcegroups.DeleteAllResources(projectName, awsSession)
-  logger.Info(str.Concat("Deleted all resources in the PAC project ", projectName))
-  logger.Warn("Route53 and Cloudfront resources are not yet supported by this command and were not deleted")
+  logger.Info(str.Concat("Finished deleting all AWS resources tagged as part of PAC project ", projectName))
+  // AWS Resource Groups does not have the ability to find certain types of
+  // resources even if they are properly tagged. This is why Route53 and
+  // Cloudfront are handled separately below
+  fqdn := str.Concat(projectName, ".pac.pyramidchallenges.com.")
+  route53.DeleteHostedZone(fqdn, awsSession)
+  route53.DeleteRecord("pac.pyramidchallenges.com", fqdn, awsSession)
+  logger.Info("Finished deleting Route53 resources")
+  cloudfront.DisableDistribution(str.Concat("integration.", projectName, ".pac.pyramidchallenges.com"), awsSession)
+  cloudfront.DisableDistribution(str.Concat("demo.", projectName, ".pac.pyramidchallenges.com"), awsSession)
+  logger.Info("Finished disabling the Cloudfront distributions")
+  logger.Warn("    Cloudfront distributions have been disabled, not deleted")
+  logger.Warn("    There is a Cloudfront origin access identity that PAC does not yet support deleting")
 }
