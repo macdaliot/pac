175ec3318fca0d1763684f3fa4cb405fa0b31475
Jeff Diederiks <jeff.diederiks@gmail.com>
2019-02-13 22:11:58 -0500

    First attempt at fully deleting Cloudfront resources

--- cmd/cleanCloud/deleteAllResources.go
+++ cmd/cleanCloud/deleteAllResources.go
@@ -5,6 +5,7 @@ import (
   "github.com/PyramidSystemsInc/go/aws/cloudfront"
   "github.com/PyramidSystemsInc/go/aws/resourcegroups"
   "github.com/PyramidSystemsInc/go/aws/route53"
+  "github.com/PyramidSystemsInc/go/commands"
   "github.com/PyramidSystemsInc/go/logger"
   "github.com/PyramidSystemsInc/go/str"
 )
@@ -12,19 +13,29 @@ import (
 func DeleteAllResources(projectName string) {
   region := "us-east-2"
   awsSession := aws.CreateAwsSession(region)
+  parentHostedZone := "pac.pyramidchallenges.com"
+  fqdn := str.Concat(projectName, ".pac.pyramidchallenges.com")
+  integrationFqdn := str.Concat("integration.", fqdn)
+  demoFqdn := str.Concat("demo.", fqdn)
+  aliases := str.Concat(integrationFqdn, " ", demoFqdn)
+  go deleteDistributions(aliases)
   resourcegroups.Create(projectName, "pac-project-name", projectName, awsSession)
   resourcegroups.DeleteAllResources(projectName, awsSession)
   logger.Info(str.Concat("Finished deleting all AWS resources tagged as part of PAC project ", projectName))
   // AWS Resource Groups does not have the ability to find certain types of
   // resources even if they are properly tagged. This is why Route53 and
   // Cloudfront are handled separately below
-  fqdn := str.Concat(projectName, ".pac.pyramidchallenges.com.")
   route53.DeleteHostedZone(fqdn, awsSession)
-  route53.DeleteRecord("pac.pyramidchallenges.com", fqdn, awsSession)
+  route53.DeleteRecord(parentHostedZone, fqdn, awsSession)
   logger.Info("Finished deleting Route53 resources")
-  cloudfront.DisableDistribution(str.Concat("integration.", projectName, ".pac.pyramidchallenges.com"), awsSession)
-  cloudfront.DisableDistribution(str.Concat("demo.", projectName, ".pac.pyramidchallenges.com"), awsSession)
+  cloudfront.DisableDistribution(integrationFqdn, awsSession)
+  cloudfront.DisableDistribution(demoFqdn, awsSession)
   logger.Info("Finished disabling the Cloudfront distributions")
   logger.Warn("    Cloudfront distributions have been disabled, not deleted")
   logger.Warn("    There is a Cloudfront origin access identity that PAC does not yet support deleting")
+}
+
+func deleteDistributions(aliases string) {
+  commands.Run("go get -u github.com/PyramidSystemsInc/delete-cloudfront-resources", "")
+  commands.Run(str.Concat("go run github.com/PyramidSystemsInc/delete-cloudfront-resources -- ", aliases), "")
 }
