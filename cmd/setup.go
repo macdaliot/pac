2a67b8803c59c0fced3bfa99852859d883ab7cc0
Michael McNairy <mmcnairy@psi-it.com>
2019-03-11 15:17:57 -0400

    removed aws creation functions

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,57 +1,55 @@
 package cmd
 
 import (
-  "github.com/PyramidSystemsInc/pac/cmd/setup"
-  "github.com/PyramidSystemsInc/go/errors"
-  "github.com/PyramidSystemsInc/go/logger"
-  "github.com/spf13/cobra"
+	"github.com/PyramidSystemsInc/go/errors"
+	"github.com/PyramidSystemsInc/go/logger"
+	"github.com/PyramidSystemsInc/pac/cmd/setup"
+	"github.com/spf13/cobra"
 )
 
 var projectDirectory string
 
 var setupCmd = &cobra.Command{
-  Use:   "setup",
-  Short: "Setup a new project",
-  Long: `Generate a new project with PAC (The default stack is a ReactJS front-end,
+	Use:   "setup",
+	Short: "Setup a new project",
+	Long: `Generate a new project with PAC (The default stack is a ReactJS front-end,
 NodeJS/Express back-end, and DynamoDB database)`,
-  Run: func(cmd *cobra.Command, args []string) {
-    logger.SetLogLevel("info")
-    projectName := getProjectName(cmd)
-    description := getDescription(cmd)
-    hostedZone := getHostedZone(cmd)
-    frontEnd := getFrontEnd(cmd)
-    backEnd := getBackEnd(cmd)
-    database := getDatabase(cmd)
-    warnExtraArgumentsAreIgnored(args)
-    setup.ValidateInputs(projectName, frontEnd, backEnd, database)
-    setup.Templates(projectName, description, gitAuth)
-    setup.Route53HostedZone(projectName, hostedZone)
-    setup.S3Buckets(projectName)
-    setup.ElasticLoadBalancer(projectName)
-    setup.Jenkins(projectName)
-    setup.SonarQube(projectName)
-    setup.Selenium(projectName)
-    setup.HaProxy(projectName)
-    setup.GitRepository(projectName)
-    setup.GitHubWebhook()
-  },
+	Run: func(cmd *cobra.Command, args []string) {
+		logger.SetLogLevel("info")
+		projectName := getProjectName(cmd)
+		description := getDescription(cmd)
+		frontEnd := getFrontEnd(cmd)
+		backEnd := getBackEnd(cmd)
+		database := getDatabase(cmd)
+		warnExtraArgumentsAreIgnored(args)
+		setup.ValidateInputs(projectName, frontEnd, backEnd, database)
+		setup.Templates(projectName, description, gitAuth)
+		// setup terraform bucket to store state
+		setup.S3Buckets(projectName)
+		// local developent via docker
+		setup.HaProxy(projectName)
+		// create github repository
+		setup.GitRepository(projectName)
+		// creates webhook to talk to Jenkins in AWS
+		setup.GitHubWebhook()
+	},
 }
 
 func init() {
-  RootCmd.AddCommand(setupCmd)
-  setupCmd.PersistentFlags().StringVarP(&projectName, "name", "n", "", "project name (required)")
-  setupCmd.MarkPersistentFlagRequired("name")
-  setupCmd.PersistentFlags().StringVar(&description, "description", "Project created by PAC", "short description of the project")
-  setupCmd.PersistentFlags().StringVar(&hostedZone, "hostedZone", "pac.pyramidchallenges.com", "Existing AWS hosted zone FQDN (i.e. pac.pyramidchallenges.com)")
-  setupCmd.PersistentFlags().StringVarP(&frontEnd, "front", "f", "ReactJS", "front-end framework/library")
-  setupCmd.PersistentFlags().StringVarP(&backEnd, "back", "b", "Express", "back-end framework/library")
-  setupCmd.PersistentFlags().StringVarP(&database, "database", "d", "DynamoDB", "database type")
+	RootCmd.AddCommand(setupCmd)
+	setupCmd.PersistentFlags().StringVarP(&projectName, "name", "n", "", "project name (required)")
+	setupCmd.MarkPersistentFlagRequired("name")
+	setupCmd.PersistentFlags().StringVar(&description, "description", "Project created by PAC", "short description of the project")
+	setupCmd.PersistentFlags().StringVar(&hostedZone, "hostedZone", "pac.pyramidchallenges.com", "Existing AWS hosted zone FQDN (i.e. pac.pyramidchallenges.com)")
+	setupCmd.PersistentFlags().StringVarP(&frontEnd, "front", "f", "ReactJS", "front-end framework/library")
+	setupCmd.PersistentFlags().StringVarP(&backEnd, "back", "b", "Express", "back-end framework/library")
+	setupCmd.PersistentFlags().StringVarP(&database, "database", "d", "DynamoDB", "database type")
 }
 
 func warnExtraArgumentsAreIgnored(args []string) {
-  if len(args) > 0 {
-    logger.Warn("Arguments were provided, but all arguments after 'setup' and before the flags are ignored")
-  }
+	if len(args) > 0 {
+		logger.Warn("Arguments were provided, but all arguments after 'setup' and before the flags are ignored")
+	}
 }
 
 var gitAuth string = "amRpZWRlcmlrc0Bwc2ktaXQuY29tOkRpZWRyZV4yMDE4"
@@ -59,49 +57,49 @@ var gitAuth string = "amRpZWRlcmlrc0Bwc2ktaXQuY29tOkRpZWRyZV4yMDE4"
 var projectName string
 
 func getProjectName(cmd *cobra.Command) string {
-  projectName, err := cmd.Flags().GetString("name")
-  errors.QuitIfError(err)
-  return projectName
+	projectName, err := cmd.Flags().GetString("name")
+	errors.QuitIfError(err)
+	return projectName
 }
 
 var description string
 
 func getDescription(cmd *cobra.Command) string {
-  description, err := cmd.Flags().GetString("description")
-  errors.QuitIfError(err)
-  return description
+	description, err := cmd.Flags().GetString("description")
+	errors.QuitIfError(err)
+	return description
 }
 
 var hostedZone string
 
 func getHostedZone(cmd *cobra.Command) string {
-  hostedZone, err := cmd.Flags().GetString("hostedZone")
-  errors.QuitIfError(err)
-  return hostedZone
+	hostedZone, err := cmd.Flags().GetString("hostedZone")
+	errors.QuitIfError(err)
+	return hostedZone
 }
 
 var frontEnd string
 
 func getFrontEnd(cmd *cobra.Command) string {
-  frontEnd, err := cmd.Flags().GetString("front")
-  errors.QuitIfError(err)
-  return frontEnd
+	frontEnd, err := cmd.Flags().GetString("front")
+	errors.QuitIfError(err)
+	return frontEnd
 }
 
 var backEnd string
 
 func getBackEnd(cmd *cobra.Command) string {
-  backEnd, err := cmd.Flags().GetString("back")
-  errors.QuitIfError(err)
-  return backEnd
+	backEnd, err := cmd.Flags().GetString("back")
+	errors.QuitIfError(err)
+	return backEnd
 }
 
 var database string
 
 func getDatabase(cmd *cobra.Command) string {
-  database, err := cmd.Flags().GetString("database")
-  errors.QuitIfError(err)
-  return database
+	database, err := cmd.Flags().GetString("database")
+	errors.QuitIfError(err)
+	return database
 }
 
 var path string
