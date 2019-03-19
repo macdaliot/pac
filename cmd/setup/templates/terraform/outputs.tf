1c7fe3d85de3f1a2ad4044d512f1e6a97f6411ac
Michael McNairy <mmcnairy@psi-it.com>
2019-03-19 14:59:42 -0400

    added outputs used by add svc

--- cmd/setup/templates/terraform/outputs.tf
+++ cmd/setup/templates/terraform/outputs.tf
@@ -1,7 +1,15 @@
 output "aws_lb_listener_api_arn" {
-    value = "aws_lb_listener.api.arn"
+    value = "${aws_lb_listener.api.id}"
+}
+
+output "aws_alb_target_group_jenkins" {
+    value = "${aws_alb_target_group.api.id}"
 }
 
 output "elasticsearch_created" {
   value = "${var.create_elasticsearch == "true" ? "Elasticsearch created" : "Elasticsearch not created" }"
+}
+
+output "pac_lambda_execution_role_arn" {
+  value = "${aws_iam_role.pac_lambda_execution_role.arn}"
 }
