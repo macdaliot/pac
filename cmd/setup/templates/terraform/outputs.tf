ffb120b185d24b0f0d205dbfdce1b4c48f6ec78f
Michael McNairy <mmcnairy@psi-it.com>
2019-03-28 11:39:49 -0400

    updated outputs

--- cmd/setup/templates/terraform/outputs.tf
+++ cmd/setup/templates/terraform/outputs.tf
@@ -1,7 +1,10 @@
-# output "aws_lb_listener_api_arn" {
-#     value = "${aws_lb_listener.api.id}"
-# }
+output "aws_lb_listener_api_arn" {
+    value = "${aws_lb_listener.api.id}"
+}
 
+output "application_vpc_id" {
+  value = "${aws_vpc.application.id}"
+}
 output "aws_lb_target_group_jenkins" {
     value = "${aws_lb_target_group.jenkins.id}"
 }
@@ -10,6 +13,6 @@ output "elasticsearch_created" {
   value = "${var.create_elasticsearch == "true" ? "Elasticsearch created" : "Elasticsearch not created" }"
 }
 
-# output "pac_lambda_execution_role_arn" {
-#   value = "${aws_iam_role.pac_lambda_execution_role.arn}"
-# }
+output "pac_lambda_execution_role_arn" {
+  value = "${aws_iam_role.pac_lambda_execution_role.arn}"
+}
