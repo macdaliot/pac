f12cba98db45cbff9e1d522a64ee19caedb09ed7
Michael McNairy <mmcnairy@psi-it.com>
2019-03-29 17:27:54 -0400

    update secret names paths for ssm

--- cmd/setup/templates/terraform/secrets.tf
+++ cmd/setup/templates/terraform/secrets.tf
@@ -11,6 +11,7 @@ variable "secrets" {
     "postgres_password",
     "sonar_jdbc_password",
     "sonar_jdbc_username",
+    "sonar_secret",
     "jenkins_password",
   ]
 }
@@ -28,7 +29,7 @@ resource "random_string" "password" {
 # https://www.terraform.io/docs/providers/aws/r/ssm_parameter.html
 #
 resource "aws_ssm_parameter" "jwt_issuer" {
-  name  = "/pac/${var.project_name}/jwt_issuer"
+  name  = "/pac/${var.project_name}/jwt/issuer"
   description = "JWT Issuer"
   type  = "SecureString"
   value = "urn:pacAuth"
@@ -36,7 +37,7 @@ resource "aws_ssm_parameter" "jwt_issuer" {
 }
 
 resource "aws_ssm_parameter" "jwt_secret" {
-  name  = "/pac/${var.project_name}/jwt_secret"
+  name  = "/pac/${var.project_name}/jwt/secret"
   description = "Test password"
   type  = "SecureString"
   value = "${random_string.password.0.result}"
@@ -50,7 +51,7 @@ resource "aws_ssm_parameter" "postgres_password" {
   key_id = "${data.aws_kms_key.project_key.key_id}"
 }
 resource "aws_ssm_parameter" "sonar_jdbc_password" {
-  name  = "/pac/${var.project_name}/sonar_jdbc_password"
+  name  = "/pac/${var.project_name}/sonar/sonar_jdbc_password"
   description = "Sonar JDBC password"
   type  = "SecureString"
   value = "sonar"
@@ -58,7 +59,7 @@ resource "aws_ssm_parameter" "sonar_jdbc_password" {
 }
 
 resource "aws_ssm_parameter" "sonar_jdbc_url" {
-  name  = "/pac/${var.project_name}/sonar_jdbc_url"
+  name  = "/pac/${var.project_name}/sonar/sonar_jdbc_url"
   description = "Sonar JDBC URL"
   type  = "SecureString"
   value = "jdbc:postgresql://localhost/sonar"
@@ -66,10 +67,18 @@ resource "aws_ssm_parameter" "sonar_jdbc_url" {
 }
 
 resource "aws_ssm_parameter" "sonar_jdbc_username" {
-  name  = "/pac/${var.project_name}/sonar_jdbc_username"
+  name  = "/pac/${var.project_name}/sonar/sonar_jdbc_username"
   description = "Sonar JDBC Username"
   type  = "SecureString"
   value = "${random_string.password.3.result}"
+  key_id = "${data.aws_kms_key.project_key.key_id}"
+}
+
+resource "aws_ssm_parameter" "sonar_secret" {
+  name  = "/pac/${var.project_name}/sonar/secret"
+  description = "Sonar Secret for Jenkins"
+  type  = "SecureString"
+  value = "${random_string.password.4.result}"
   key_id = "${data.aws_kms_key.project_key.key_id}"
 }
 
@@ -85,7 +94,7 @@ resource "aws_ssm_parameter" "jenkins_password" {
   name  = "/pac/${var.project_name}/jenkins/password"
   description = "Jenkins default password"
   type  = "SecureString"
-  value = "${random_string.password.4.result}"
+  value = "${random_string.password.5.result}"
   key_id = "${data.aws_kms_key.project_key.key_id}"
 }
 
