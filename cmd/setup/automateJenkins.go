9e498c39a8d13ff36f832e0f95deadd8fcdcaca6
Michael McNairy <mmcnairy@psi-it.com>
2019-04-25 14:25:28 -0400

    fixed automate jenkins

--- cmd/setup/automateJenkins.go
+++ cmd/setup/automateJenkins.go
@@ -1,54 +1,54 @@
 package setup
 
 import (
-  "time"
+	"time"
 
-  "github.com/PyramidSystemsInc/go/commands"
-  "github.com/PyramidSystemsInc/go/errors"
-  "github.com/PyramidSystemsInc/go/files"
-  "github.com/PyramidSystemsInc/go/logger"
-  "github.com/PyramidSystemsInc/go/str"
-  "github.com/PyramidSystemsInc/pac/config"
+	"github.com/PyramidSystemsInc/go/commands"
+	"github.com/PyramidSystemsInc/go/errors"
+	"github.com/PyramidSystemsInc/go/files"
+	"github.com/PyramidSystemsInc/go/logger"
+	"github.com/PyramidSystemsInc/go/str"
+	"github.com/PyramidSystemsInc/pac/config"
 )
 
 func AutomateJenkins() {
-  time.Sleep(60 * time.Second)
-  projectName := config.Get("projectName")
-  jenkinsUrl := config.Get("jenkinsUrl")
-  DownloadJenkinsCliJar(jenkinsUrl)
-  createEntrypointJobXml(projectName)
-  createMasterPipelineXml(projectName)
-  createFrontEndPipelineXml(projectName)
-  createServicesPipelineXml(projectName)
-  jenkinsCliCommandStart := str.Concat("java -jar jenkins-cli.jar -s http://", jenkinsUrl, " -auth pyramid:systems")
-  createPipelineJobs(jenkinsUrl, projectName, jenkinsCliCommandStart)
-  cleanUp()
-  logger.Info("Jenkins should now be completely configured.")
+	time.Sleep(60 * time.Second)
+	projectName := config.Get("projectName")
+	jenkinsURL := config.Get("jenkinsURL")
+	DownloadJenkinsCliJar(jenkinsURL)
+	createEntrypointJobXml(projectName)
+	createMasterPipelineXml(projectName)
+	createFrontEndPipelineXml(projectName)
+	createServicesPipelineXml(projectName)
+	jenkinsCliCommandStart := str.Concat("java -jar jenkins-cli.jar -s https://", jenkinsURL, " -auth pyramid:systems")
+	createPipelineJobs(jenkinsURL, projectName, jenkinsCliCommandStart)
+	cleanUp()
+	logger.Info("Jenkins should now be completely configured.")
 }
 
-func DownloadJenkinsCliJar(jenkinsUrl string) {
-  corruptJenkinsCliError := "Error: Invalid or corrupt jarfile jenkins-cli.jar"
-  jenkinsCliPath := "./jenkins-cli.jar"
-  err := files.Download(str.Concat("http://", jenkinsUrl, "/jnlpJars/jenkins-cli.jar"), jenkinsCliPath)
-  if err != nil {
-    logger.Info("Made it in the if")
-    if err.Error() == corruptJenkinsCliError {
-      logger.Info("Made it in the nested if")
-      files.Delete(jenkinsCliPath)
-      logger.Info("Received a corrupt Jenkins CLI. Attempting another download")
-      time.Sleep(20 * time.Second)
-      DownloadJenkinsCliJar(jenkinsUrl)
-    } else {
-      errors.LogAndQuit(str.Concat("Downloading the Jenkins CLI failed with the following error: ", err.Error()))
-    }
-  }
+func DownloadJenkinsCliJar(jenkinsURL string) {
+	corruptJenkinsCliError := "Error: Invalid or corrupt jarfile jenkins-cli.jar"
+	jenkinsCliPath := "./jenkins-cli.jar"
+	err := files.Download(str.Concat("https://", jenkinsURL, "/jnlpJars/jenkins-cli.jar"), jenkinsCliPath)
+	if err != nil {
+		logger.Info("Made it in the if")
+		if err.Error() == corruptJenkinsCliError {
+			logger.Info("Made it in the nested if")
+			files.Delete(jenkinsCliPath)
+			logger.Info("Received a corrupt Jenkins CLI. Attempting another download")
+			time.Sleep(20 * time.Second)
+			DownloadJenkinsCliJar(jenkinsURL)
+		} else {
+			errors.LogAndQuit(str.Concat("Downloading the Jenkins CLI failed with the following error: ", err.Error()))
+		}
+	}
 }
 
 func createEntrypointJobXml(projectName string) {
-  filePath := "entrypoint-job.xml"
-  config := make(map[string]string)
-  config["projectName"] = projectName
-  const template = `<?xml version='1.1' encoding='UTF-8'?>
+	filePath := "entrypoint-job.xml"
+	config := make(map[string]string)
+	config["projectName"] = projectName
+	const template = `<?xml version='1.1' encoding='UTF-8'?>
 <project>
   <actions/>
   <description></description>
@@ -93,21 +93,21 @@ func createEntrypointJobXml(projectName string) {
   <concurrentBuild>false</concurrentBuild>
   <builders>
     <hudson.tasks.Shell>
-      <command>java -jar ~/jenkins-cli.jar -s http://localhost:8080 -auth pyramid:systems build master</command>
+      <command>java -jar ~/jenkins-cli.jar -s https://localhost:8080 -auth pyramid:systems build master</command>
     </hudson.tasks.Shell>
   </builders>
   <publishers/>
   <buildWrappers />
 </project>
 `
-  files.CreateFromTemplate(filePath, template, config)
+	files.CreateFromTemplate(filePath, template, config)
 }
 
 func createServicesPipelineXml(projectName string) {
-  filePath := "services-pipeline.xml"
-  config := make(map[string]string)
-  config["projectName"] = projectName
-  const template = `<?xml version="1.1" encoding="UTF-8"?><flow-definition plugin="workflow-job@2.31">
+	filePath := "services-pipeline.xml"
+	config := make(map[string]string)
+	config["projectName"] = projectName
+	const template = `<?xml version="1.1" encoding="UTF-8"?><flow-definition plugin="workflow-job@2.31">
   <description/>
   <keepDependencies>false</keepDependencies>
   <properties>
@@ -141,14 +141,14 @@ func createServicesPipelineXml(projectName string) {
   <disabled>false</disabled>
 </flow-definition>
 `
-  files.CreateFromTemplate(filePath, template, config)
+	files.CreateFromTemplate(filePath, template, config)
 }
 
 func createFrontEndPipelineXml(projectName string) {
-  filePath := "front-end-pipeline.xml"
-  config := make(map[string]string)
-  config["projectName"] = projectName
-  const template = `<?xml version="1.1" encoding="UTF-8"?><flow-definition plugin="workflow-job@2.31">
+	filePath := "front-end-pipeline.xml"
+	config := make(map[string]string)
+	config["projectName"] = projectName
+	const template = `<?xml version="1.1" encoding="UTF-8"?><flow-definition plugin="workflow-job@2.31">
   <description/>
   <keepDependencies>false</keepDependencies>
   <properties>
@@ -191,14 +191,14 @@ func createFrontEndPipelineXml(projectName string) {
   <disabled>false</disabled>
 </flow-definition>
 `
-  files.CreateFromTemplate(filePath, template, config)
+	files.CreateFromTemplate(filePath, template, config)
 }
 
 func createMasterPipelineXml(projectName string) {
-  filePath := "master-pipeline.xml"
-  config := make(map[string]string)
-  config["projectName"] = projectName
-  const template = `<?xml version="1.1" encoding="UTF-8"?><flow-definition plugin="workflow-job@2.31">
+	filePath := "master-pipeline.xml"
+	config := make(map[string]string)
+	config["projectName"] = projectName
+	const template = `<?xml version="1.1" encoding="UTF-8"?><flow-definition plugin="workflow-job@2.31">
   <description/>
   <keepDependencies>false</keepDependencies>
   <properties>
@@ -241,25 +241,25 @@ func createMasterPipelineXml(projectName string) {
   <disabled>false</disabled>
 </flow-definition>
 `
-  files.CreateFromTemplate(filePath, template, config)
+	files.CreateFromTemplate(filePath, template, config)
 }
 
-func createPipelineJobs(jenkinsUrl string, projectName string, jenkinsCliCommandStart string) {
-  jobData := files.Read("entrypoint-job.xml")
-  createJobCommand := str.Concat(jenkinsCliCommandStart, " create-job entrypoint")
-  commands.RunWithStdin(createJobCommand, string(jobData), "")
-  jobData = files.Read("services-pipeline.xml")
-  createJobCommand = str.Concat(jenkinsCliCommandStart, " create-job services")
-  commands.RunWithStdin(createJobCommand, string(jobData), "")
-  jobData = files.Read("front-end-pipeline.xml")
-  createJobCommand = str.Concat(jenkinsCliCommandStart, " create-job front-end")
-  commands.RunWithStdin(createJobCommand, string(jobData), "")
-  jobData = files.Read("master-pipeline.xml")
-  createJobCommand = str.Concat(jenkinsCliCommandStart, " create-job master")
-  commands.RunWithStdin(createJobCommand, string(jobData), "")
+func createPipelineJobs(jenkinsURL string, projectName string, jenkinsCliCommandStart string) {
+	jobData := files.Read("entrypoint-job.xml")
+	createJobCommand := str.Concat(jenkinsCliCommandStart, " create-job entrypoint")
+	commands.RunWithStdin(createJobCommand, string(jobData), "")
+	jobData = files.Read("services-pipeline.xml")
+	createJobCommand = str.Concat(jenkinsCliCommandStart, " create-job services")
+	commands.RunWithStdin(createJobCommand, string(jobData), "")
+	jobData = files.Read("front-end-pipeline.xml")
+	createJobCommand = str.Concat(jenkinsCliCommandStart, " create-job front-end")
+	commands.RunWithStdin(createJobCommand, string(jobData), "")
+	jobData = files.Read("master-pipeline.xml")
+	createJobCommand = str.Concat(jenkinsCliCommandStart, " create-job master")
+	commands.RunWithStdin(createJobCommand, string(jobData), "")
 }
 
 func cleanUp() {
-  // TODO: Change to os.Rm() or something in order to support Windows CMD
-  commands.Run("rm jenkins-cli.jar entrypoint-job.xml front-end-pipeline.xml services-pipeline.xml master-pipeline.xml", "")
+	// TODO: Change to os.Rm() or something in order to support Windows CMD
+	commands.Run("rm jenkins-cli.jar entrypoint-job.xml front-end-pipeline.xml services-pipeline.xml master-pipeline.xml", "")
 }
