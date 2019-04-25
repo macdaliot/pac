6be1e282b93616b9981801d44fe58bbdb53addfa
Michael McNairy <mmcnairy@psi-it.com>
2019-04-25 11:07:57 -0400

    added lifecycle block per TF doc recommendation

--- cmd/setup/templates/terraform/ssl.tf
+++ cmd/setup/templates/terraform/ssl.tf
@@ -37,9 +37,14 @@ resource "aws_acm_certificate" "main" {
   domain_name = "${var.domain_names[0]}"
   subject_alternative_names = "${slice(var.domain_names, 1, length(var.domain_names))}"
   validation_method = "DNS"
+  
   tags {
     Name = "${replace(var.domain_names[0], "*.", "star.")}"
     terraform = "true"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
