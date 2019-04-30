c33465cd0c2c9e7565c74354b725d9d6d342313c
Mike Shields <mshields@pyramidsystems.com>
2019-04-30 12:58:34 -0400

    add OS X support for pac install (no windows yet)

--- cmd/install.go
+++ cmd/install.go
@@ -1,44 +1,44 @@
 package cmd
 
 import (
-  "strings"
+	"strings"
 
-  "github.com/PyramidSystemsInc/go/commands"
-  "github.com/PyramidSystemsInc/go/errors"
-  "github.com/PyramidSystemsInc/go/logger"
-  "github.com/PyramidSystemsInc/go/str"
-  "github.com/PyramidSystemsInc/pac/util"
-  "github.com/spf13/cobra"
+	"github.com/PyramidSystemsInc/go/commands"
+	"github.com/PyramidSystemsInc/go/errors"
+	"github.com/PyramidSystemsInc/go/logger"
+	"github.com/PyramidSystemsInc/go/str"
+	"github.com/PyramidSystemsInc/pac/util"
+	"github.com/spf13/cobra"
 )
 
 var installCmd = &cobra.Command{
-  Use:   "install",
-  Short: "Installs all project dependencies",
-  Long: `Finds all package.json files within a project and runs 'npm install'
+	Use:   "install",
+	Short: "Installs all project dependencies",
+	Long: `Finds all package.json files within a project and runs 'npm install'
 on each directory`,
-  Run: func(cmd *cobra.Command, args []string) {
-    logger.SetLogLevel("info")
+	Run: func(cmd *cobra.Command, args []string) {
+		logger.SetLogLevel("info")
 
-    // Warn the user that any additional arguments provided will be ignored
-    warnExtraArgumentsAreIgnored(args)
+		// Warn the user that any additional arguments provided will be ignored
+		warnExtraArgumentsAreIgnored(args)
 
-    // Run `npm install` for each Node project directory within the PAC project
-    util.GoToRootDirectory()
-    packageJsonLocations, err := commands.Run("find -name package.json", "")
-    errors.QuitIfError(err)
-    str.RunForEachLine(performNpmInstall, packageJsonLocations)
-  },
+		// Run `npm install` for each Node project directory within the PAC project
+		util.GoToRootDirectory()
+		packageJsonLocations, err := commands.Run("find . -name package.json", "")
+		errors.QuitIfError(err)
+		str.RunForEachLine(performNpmInstall, packageJsonLocations)
+	},
 }
 
 func init() {
-  RootCmd.AddCommand(installCmd)
+	RootCmd.AddCommand(installCmd)
 }
 
 func performNpmInstall(packageJsonLocation string) {
-  if !strings.Contains(packageJsonLocation, "node_modules") {
-    subProjectDirectory := strings.TrimSuffix(packageJsonLocation, "/package.json")
-    _, err := commands.Run("npm i", subProjectDirectory)
-    errors.QuitIfError(err)
-    logger.Info(str.Concat("Installed node modules at ", subProjectDirectory))
-  }
+	if !strings.Contains(packageJsonLocation, "node_modules") {
+		subProjectDirectory := strings.TrimSuffix(packageJsonLocation, "/package.json")
+		_, err := commands.Run("npm i", subProjectDirectory)
+		errors.QuitIfError(err)
+		logger.Info(str.Concat("Installed node modules at ", subProjectDirectory))
+	}
 }
