43f01ebc230d79b723d5a8bc3df1f35f93c6c3e9
Stephen Hu <shu@psi-it.com>
2019-05-02 13:24:40 -0400

    backported tests and some documentation from inttests

--- cmd/add/service/serviceTemplates/src/server.ts
+++ cmd/add/service/serviceTemplates/src/server.ts
@@ -8,8 +8,8 @@ import * as passport from 'passport';
 import { jwtStrategy, errorMiddleware, } from '@pyramid-systems/core';
 import * as swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../docs/swagger.json';
-import { RegisterRoutes } from './generated/routes.js';
-import { setupContainer } from './container-setup.js';
+import { RegisterRoutes } from './generated/routes';
+import { setupContainer } from './container-setup';
 
 passport.use(jwtStrategy);
 const app = express();
@@ -18,7 +18,7 @@ app.use(cors())
     .use(passport.initialize())
     .use(express.json())
     .use(express.urlencoded({ extended: false }))
-    .use('/api/{{.serviceName}}/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    .use('/api/gizmos/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 RegisterRoutes(app, container);
 
