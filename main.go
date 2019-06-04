5b60d9615b07dd1f9546391ee810d68831dd7247
Michael McNairy <mmcnairy@psi-it.com>
2019-06-04 10:11:57 -0400

    rm special character from dynamoDbToElasticSearch

--- main.go
+++ main.go
@@ -4,14 +4,4 @@ import "github.com/PyramidSystemsInc/pac/cmd"
 
 func main() {
 	cmd.Execute()
-	// m := config.ReadAll()
-
-	// m["region"] = "us-west-2"
-	// m["projectName"] = "myproject"
-
-	// config.Set("region", "us-west-2")
-
-	// for k, v := range m {
-	// 	fmt.Printf("%s : %s\n", k, v)
-	// }
 }
