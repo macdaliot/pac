7d2ee72549070710f70839fc4ff96e690e0ad9cc
Michael McNairy <mmcnairy@psi-it.com>
2019-06-06 14:05:40 -0400

    updated remote_terraform_data from pac to management

--- cmd/add/authservice/authTerraformTemplates/lambda_auth.tf
+++ cmd/add/authservice/authTerraformTemplates/lambda_auth.tf
@@ -16,7 +16,7 @@ resource "aws_lambda_function" "lambda_auth" {
   s3_bucket        = "lambda.${var.project_name}.${var.hosted_zone}"
   s3_key           = "auth.zip"
   function_name    = "pac-{{ .projectName }}-i-auth"
-  role             = "${data.terraform_remote_state.pac.{{ .projectName }}_lambda_execution_role_arn}"
+  role             = "${data.terraform_remote_state.management.{{ .projectName }}_lambda_execution_role_arn}"
   handler          = "lambda.handler"
   # source_code_hash = "${base64sha256(file(var.lambda_function_payload))}"
   runtime          = "nodejs8.10"
@@ -28,7 +28,7 @@ resource "aws_lambda_function" "lambda_auth" {
       APP_ROOT        = "http://integration.{{ .projectName }}.pac.pyramidchallenges.com.s3-website.{{ .region }}.amazonaws.com"
       AUTH0_CLIENT_ID = "PJqs70Pr0VRH67aR2TnHf4Sn6DDldiNR"
       AUTH0_DOMAIN    = "pyramidsystems.auth0.com"
-      JWT_SECRET      = "${data.terraform_remote_state.pac.jwt_secret}"
+      JWT_SECRET      = "${data.terraform_remote_state.management.jwt_secret}"
       SAML_CALLBACK   = "https://api.{{ .projectName }}.pac.pyramidchallenges.com/api/auth/callback"
       SAML_SIGNER     = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tDQpNSUlDK0RDQ0FlQ2dBd0lCQWdJSlBPV29wemhNQVdPSk1BMEdDU3FHU0liM0RRRUJDd1VBTUNNeElUQWZCZ05WDQpCQU1UR0hCNWNtRnRhV1J6ZVhOMFpXMXpMbUYxZEdnd0xtTnZiVEFlRncweE56QXpNVEV4T0RJd01EbGFGdzB6DQpNREV4TVRneE9ESXdNRGxhTUNNeElUQWZCZ05WQkFNVEdIQjVjbUZ0YVdSemVYTjBaVzF6TG1GMWRHZ3dMbU52DQpiVENDQVNJd0RRWUpLb1pJaHZjTkFRRUJCUUFEZ2dFUEFEQ0NBUW9DZ2dFQkFNa2pFeDkyZkFscmU0S0xYUXgvDQplQ2lUTFZOY2ljMnlUeXRRb2cveWJsRzJvMEx5L0lGdHhaTmpFaEF2ODQ1MGpiWDdFVG1iNkhrUEZWb0FHcDNsDQp6WGdxNTByL3pRSzRmMVp6Qzc1VC9YL01HVlF3TmRGNDZXUEJlaUs4T0xndzVZa2RUa1J6V1pDQm9zNGRMK2NZDQoyWHc0eGpZQ1MxL28wMExPZzVjaDdCTEtodTRSb0dRRnRWRUZBV0VTTytBZ2xCRVFSR2ljbXQ2WWhVTW5RdTV6DQo4bHdvTXRRNFlZdk9vNVpsbUdLZ2hEOEJzTmhidGdLRkV2UVlwbXpmZVQ5amRScjhGOHN6cHkya0xmTU0wRWMzDQo4cEEvNnFialA0ckhHdzE5ZS9FUmhyRlRTa2FFMFMyVFkya1l0V1RFODdCSHI4SWJFaHYvNDl1OGVkbW5ERlBFDQo3aDBDQXdFQUFhTXZNQzB3REFZRFZSMFRCQVV3QXdFQi96QWRCZ05WSFE0RUZnUVVLMEFuUHpXeGhLOTZzeEUrDQoydjhCQjRBc2ZKMHdEUVlKS29aSWh2Y05BUUVMQlFBRGdnRUJBSWRtYkFzVC9tK01YcFh0MzBuMHk4dU5JcCs4DQppTFVIM3duL3RaQUl6Tjk1WDJ1ZlNuRTZSb1dWaGNzOVAyT0NScjY0b1ZzU29McUJhWjVwSkhINmdtSTVlc0lTDQp6dlRFYXZIUDIyb2QyeDUwcEVlZGpzSyt5Rlh1bkZ2b2xYYnIyeFU2bmMwc3FuSW12OWVCMU1vN3FsU0hxQnhIDQpoUkh5NXhCUGVVcTI5U0ZrM0wwd042UUhaNXNTZGRKV2prYmMvVWdFZnpGeURNQUxqZEhHUGd2ZVB6bU44SlVJDQpWVkxYOHBrYWdQYVJ4RWd2dTN6RGtTRjBBMkEvejZPMFZVWWNXWi9YN1Nad0Z4d3ZRNEpoYTR1ejFOcHJrY0s0DQptNUNkY1pSak41WWlUWUV5YmtBWWR4Z0w3M3dZUUQwYlRYVVAySkRscVlYeVFUVVdPWHFaR0N4NXRFST0NCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0NCg=="
     }
@@ -48,7 +48,7 @@ resource "aws_alb_target_group" "{{ .projectName }}_auth_target_group" {
   name        = "pac-{{ .projectName }}-i-auth"
   port        = "80"
   protocol    = "http"
-  vpc_id      = "${data.terraform_remote_state.pac.application_vpc_id}"
+  vpc_id      = "${data.terraform_remote_state.management.application_vpc_id}"
   target_type = "lambda"
 }
 
@@ -79,7 +79,7 @@ resource "aws_alb_target_group_attachment" "{{ .projectName }}_auth_target_group
 # alb listener rule
 #
 resource "aws_lb_listener_rule" "{{ .projectName }}_auth_rule_100" {
-  listener_arn = "${data.terraform_remote_state.pac.aws_lb_listener_api_arn}"
+  listener_arn = "${data.terraform_remote_state.management.aws_lb_listener_api_arn}"
   action {
     type = "forward"
     target_group_arn = "${aws_alb_target_group.{{ .projectName }}_auth_target_group.arn}"
@@ -91,7 +91,7 @@ resource "aws_lb_listener_rule" "{{ .projectName }}_auth_rule_100" {
 }
 
 resource "aws_lb_listener_rule" "{{ .projectName }}_auth_rule_200" {
-  listener_arn = "${data.terraform_remote_state.pac.aws_lb_listener_api_arn}"
+  listener_arn = "${data.terraform_remote_state.management.aws_lb_listener_api_arn}"
   action {
     type = "forward"
     target_group_arn = "${aws_alb_target_group.{{ .projectName }}_auth_target_group.arn}"
