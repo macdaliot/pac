91593f9f8abebe3c96aa045ce0c6b690085f55a6
Michael McNairy <mmcnairy@psi-it.com>
2019-06-17 09:44:58 -0400

    added keypair generation

--- cmd/setup/templates/services/terraform/elasticsearch_vpc.tf
+++ cmd/setup/templates/services/terraform/elasticsearch_vpc.tf
@@ -340,7 +340,7 @@ resource "aws_instance" "jumpbox" {
   ami                         = "${data.aws_ami.amzn.id}"
   associate_public_ip_address = true
   instance_type               = "t2.micro"
-  key_name                    = "jumpbox"
+  key_name                    = "jumpbox-${var.project_name}"
   subnet_id                   = "${aws_subnet.public.0.id}"
   vpc_security_group_ids      = ["${aws_security_group.es_jumpbox.id}"]
 
--- cmd/setup/templates/terraform/terraform.tfvars
+++ cmd/setup/templates/terraform/terraform.tfvars
@@ -1,2 +1,3 @@
-project_name         = "{{ .projectName }}"
-enable_elasticsearch = true
+project_name          = "{{ .projectName }}"
+enable_elasticsearch  = "true"
+enable_keypair_output = "false"
