1a4047d127ec310eb34efbbd2f544347616dd584
Mike Shields <mshields@pyramidsystems.com>
2019-06-18 13:14:53 -0400

    Restore new template generation code This reverts commit 09d57a035699a9f71ce34eada85c609335ddd2b6.

--- cmd/add/service/instantiateTemplates.go
+++ cmd/add/service/instantiateTemplates.go
@@ -5,12 +5,10 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 
 	"github.com/PyramidSystemsInc/go/errors"
 	"github.com/PyramidSystemsInc/go/files"
 	"github.com/PyramidSystemsInc/go/logger"
-	"github.com/PyramidSystemsInc/go/str"
 	"github.com/PyramidSystemsInc/pac/config"
 	"github.com/gobuffalo/packr"
 )
@@ -18,44 +16,7 @@ import (
 func CreateAllTemplatedFiles(cfg map[string]string) {
 	serviceName := cfg["serviceName"]
 
-	os.Chdir(config.GetRootDirectory())
-	os.Chdir(path.Join("services/", serviceName))
-
-	box := packr.NewBox("./serviceTemplates")
-	for _, templatePath := range box.List() {
-		logger.Info(templatePath)
-		files.EnsurePath(filepath.Dir(templatePath))
-		template, _ := box.FindString(templatePath)
-		if templatePath == "src/controller.ts" {
-			files.CreateFromTemplate(str.Concat("src/", serviceName, ".controller.ts"), template, cfg)
-		} else if templatePath == "src/controller.spec.ts" {
-			files.CreateFromTemplate(str.Concat("src/", serviceName, ".controller.spec.ts"), template, cfg)
-		} else {
-			files.CreateFromTemplate(templatePath, template, cfg)
-		}
-	}
-	os.Chdir(config.GetRootDirectory())
-	os.Chdir("domain")
-
-	logger.Info("Writing the domain templates")
-
-	box = packr.NewBox("./domainTemplates")
-	for _, templatePath := range box.List() {
-		logger.Info(templatePath)
-
-		template, _ := box.FindString(templatePath)
-
-		if strings.Contains(templatePath, "serviceNameFolder") {
-			templatePath = strings.Replace(templatePath, "serviceNameFolder", serviceName, -1)
-		}
-
-		files.EnsurePath(filepath.Dir(templatePath))
-
-		logger.Info("Replaced template path: " + templatePath)
-
-		files.CreateFromTemplate(templatePath, template, cfg)
-	}
-
+	// make sure we are at root dir
 	os.Chdir(config.GetRootDirectory())
 
 	// service templates
@@ -82,24 +43,9 @@ func CreateAllTemplatedFiles(cfg map[string]string) {
 	files.Append("./domain/index.ts", []byte(exportTemplate))
 	files.Append("./domain/models.ts", []byte(exportModelTemplate))
 
-	os.Chdir(config.GetRootDirectory())
-	os.Chdir("./services/terraform")
-
-	box = packr.NewBox("./terraformTemplates")
-	for _, templatePath := range box.List() {
-		logger.Info(templatePath)
-		files.EnsurePath(filepath.Dir(templatePath))
-
-		if strings.HasSuffix(templatePath, ".zip") {
-			fileContents, _ := box.Find(templatePath)
-			files.CreateFromBinary(templatePath, fileContents)
-		} else {
-			template, _ := box.FindString(templatePath)
-			files.CreateFromTemplate(templatePath, template, cfg)
-		}
-	}
-
-	os.Rename("./lambda.tf", "./lambda_"+serviceName+".tf")
-
-	os.Chdir(config.GetRootDirectory())
+	// terraform templates
+	options.Box = packr.NewBox("./terraformTemplates")
+	options.TargetDirectory = "./services/terraform"
+	err = files.CreateTemplatedFiles(options)
+	errors.QuitIfError(err)
 }
