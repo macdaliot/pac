2b7276faca285fc7fc47a695ef58e6dfae8d6876
Michael McNairy <mmcnairy@psi-it.com>
2019-10-07 12:24:09 -0400

    added flow logs to vpc

--- cmd/setup/templates/terraform/management/vpc_management.tf
+++ cmd/setup/templates/terraform/management/vpc_management.tf
@@ -1,3 +1,6 @@
+#----------------------------------------------------------------------------------------------------------------------
+# VPC
+#----------------------------------------------------------------------------------------------------------------------
 resource "aws_vpc" "management_vpc" {
   cidr_block = var.management_cidr_block
 
@@ -8,6 +11,30 @@ resource "aws_vpc" "management_vpc" {
   }
 }
 
+#----------------------------------------------------------------------------------------------------------------------
+# S3 VPC ENDPOINT
+#----------------------------------------------------------------------------------------------------------------------
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = "${aws_vpc.management_vpc.id}"
+  service_name = "com.amazonaws.${var.region}.s3"
+}
+
+#----------------------------------------------------------------------------------------------------------------------
+# VPC FLOW LOGS
+#----------------------------------------------------------------------------------------------------------------------
+module "management_vpc_flow_log" {
+  source = "../modules/vpc_flow_log"
+
+  iam_role_arn  = aws_iam_role.vpc_flow_log.arn
+  log_group_arn = aws_cloudwatch_log_group.practiceone_log_group.arn
+  project_name  = var.project_name
+  vpc_id        = aws_vpc.management_vpc.id
+}
+
+#----------------------------------------------------------------------------------------------------------------------
+# OUTPUTS
+#----------------------------------------------------------------------------------------------------------------------
+
 output "management_vpc_cidr_block" {
   value = aws_vpc.management_vpc.cidr_block
 }
@@ -15,4 +42,3 @@ output "management_vpc_cidr_block" {
 output "management_vpc_id" {
   value = aws_vpc.management_vpc.id
 }
-
