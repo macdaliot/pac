638ad666f9a0446a7adae672f8393048f2f42fba
root <root@ip-172-31-13-34.ec2.internal>
2019-10-09 18:30:33 +0000

    Added new terraform templates and configuration to management lifecycle

---
+++ cmd/setup/templates/terraform/management/elasticsearch.tf
@@ -0,0 +1,313 @@
+#
+# http://www.terraform.io/docs/providers/aws/r/elasticsearch_domain.html
+#
+resource "aws_elasticsearch_domain" "es" {
+  count                 = var.enable_elasticsearch == "true" ? 1 : 0
+  domain_name           = "${var.project_name}-management"
+  elasticsearch_version = var.es_version
+
+  cluster_config {
+    instance_type = var.es_instance_type
+  }
+
+  vpc_options {
+    subnet_ids = [
+      aws_subnet.private[0].id,
+    ]
+    security_group_ids = [aws_security_group.es[0].id]
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  snapshot_options {
+    automated_snapshot_start_hour = var.es_automated_snapshot_start_hour
+  }
+
+  tags = {
+    Domain = "${var.project_name} Elasticsearch Domain"
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
+
+output "elasticsearch_endpoint" {
+  value = aws_elasticsearch_domain.es.*.endpoint
+}
+
+resource "aws_elasticsearch_domain_policy" "vpc_based" {
+  count       = var.enable_elasticsearch == "true" ? 1 : 0
+  domain_name = aws_elasticsearch_domain.es[0].domain_name
+
+  access_policies = <<POLICIES
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": "es:*",
+      "Resource": [
+          "${aws_elasticsearch_domain.es[0].arn}/*"
+      ]
+    }
+  ]
+}
+POLICIES
+
+}
+
+resource "aws_security_group" "es" {
+  count = var.enable_elasticsearch == "true" ? 1 : 0
+  name = "elasticsearch-${var.project_name}"
+  description = "Managed by Terraform"
+  vpc_id = aws_vpc.management_vpc.id
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = [
+      aws_vpc.management_vpc.cidr_block,
+    ]
+  }
+
+  tags = {
+    Name             = var.project_name
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
+
+#----------------------------------------------------------------------------------------------------------------------
+# SEND CLOUDWATCH LOGS TO ELASTICSEARCH
+#----------------------------------------------------------------------------------------------------------------------
+
+variable "es_endpoint" {
+  type = string
+  default = "elasticsearch.endpoint.es.amazonaws.com"
+}
+
+variable "cwl_endpoint" {
+  type = string
+  default = "logs.us-east-2.amazonaws.com"
+}
+
+# CloudWatch Logs uses Lambda to deliver log data to Amazon ES. 
+# You must specify an IAM role that grants Lambda permission to make calls to Amazon ES. 
+# You can choose an existing role or create an IAM role that automatically has the required permissions. 
+# To deliver log data to another account, you must specify the Elasticsearch Domain ARN and 
+# Elasticsearch Endpoint of other account and ensure permissions are granted to be able to publish to that ARN.
+#
+resource "aws_iam_role" "lambda_elasticsearch_execution_role" {
+  count = var.enable_elasticsearch == "true" ? 1 : 0
+  name = "${var.project_name}_management_lambda_es_xrole"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    Name             = var.project_name
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
+
+# When using a non-private Elasticsearch cluster, the Lambda IAM Execution Role needs to have the following permissions 
+# policy: AWSLambdaVPCAccessExecutionRole. So we are attaching it here.
+resource "aws_iam_role_policy_attachment" "AWSLambdaVPCAccessExecutionRole" {
+count = var.enable_elasticsearch == "true" ? 1 : 0
+role  = aws_iam_role.lambda_elasticsearch_execution_role[0].name
+
+# AWS Mananged role, safe to hard code
+policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+# Creates an inline policy on the lambda_elasticsearch_execution_role that allows us to stream logs to Elasticsearch.
+resource "aws_iam_role_policy" "lambda_elasticsearch_execution_policy" {
+count  = var.enable_elasticsearch == "true" ? 1 : 0
+name   = "${var.project_name}_lambda_elasticsearch_execution_policy"
+role   = aws_iam_role.lambda_elasticsearch_execution_role[0].id
+policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "es:ESHttpPost",
+      "Resource": "${aws_elasticsearch_domain.es[0].arn}"
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_lambda_function" "cwl_stream_lambda" {
+  count         = var.enable_elasticsearch == "true" ? 1 : 0
+  filename      = "cwl2eslambda.zip"
+  function_name = "LogsToElasticsearch-${var.project_name}-management"
+  role          = aws_iam_role.lambda_elasticsearch_execution_role[0].arn
+  handler       = "exports.handler"
+
+  #source_code_hash = "${base64sha256(file("cwl2eslambda.zip"))}"
+  runtime = "nodejs8.10"
+
+  environment {
+    variables = {
+      es_endpoint = aws_elasticsearch_domain.es[0].endpoint
+    }
+  }
+
+  vpc_config {
+    subnet_ids = [aws_subnet.private[0].id, aws_subnet.private[1].id]
+    security_group_ids = [aws_vpc.management_vpc.default_security_group_id]
+  }
+
+  tags = {
+    Name             = var.project_name
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
+
+resource "aws_lambda_permission" "cloudwatch_allow" {
+  count = var.enable_elasticsearch == "true" ? 1 : 0
+  statement_id = "cloudwatch_allow"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.cwl_stream_lambda[0].arn
+  principal = var.cwl_endpoint
+  source_arn = aws_cloudwatch_log_group.practiceone_log_group.arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "cloudwatch_logs_to_es" {
+  count = var.enable_elasticsearch == "true" ? 1 : 0
+  depends_on = [aws_lambda_permission.cloudwatch_allow]
+  name = "cloudwatch_logs_to_elasticsearch-management"
+  log_group_name = aws_cloudwatch_log_group.practiceone_log_group.name
+  filter_pattern = ""
+  destination_arn = aws_lambda_function.cwl_stream_lambda[0].arn
+}
+
+#----------------------------------------------------------------------------------------------------------------------
+# DYNAMOBDB ACTIVITY TO ELASTICSEARCH
+#----------------------------------------------------------------------------------------------------------------------
+
+resource "aws_iam_policy" "practiceone_dev_dynamodb_elasticsearch" {
+  count = var.enable_elasticsearch == "true" ? 1 : 0
+  name = "${var.project_name}-management-dynamodb-es-policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Sid": "",
+          "Effect": "Allow",
+          "Action": [
+              "dynamodb:*"
+          ],
+          "Resource": "*"
+      }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_role_policy_attachment" "practiceone_dev_lambda_elasticsearch_attach_policy" {
+  count      = var.enable_elasticsearch == "true" ? 1 : 0
+  role       = aws_iam_role.lambda_elasticsearch_execution_role[0].name
+  policy_arn = aws_iam_policy.practiceone_dev_dynamodb_elasticsearch[0].arn
+}
+
+#----------------------------------------------------------------------------------------------------------------------
+# JUMPBOX TO ALLOW ACCESS TO KIBANA
+#
+# https://www.jeremydaly.com/access-aws-vpc-based-elasticsearch-cluster-locally/
+# https://docs.aws.amazon.com/elasticsearch-service/latest/developerguide/es-vpc.html
+# https://forums.aws.amazon.com/thread.jspa?threadID=279437
+#----------------------------------------------------------------------------------------------------------------------
+
+data "aws_ami" "amzn" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = [var.jumpbox_ami]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["137112412989"] # Amazon
+}
+
+resource "aws_security_group" "es_jumpbox" {
+  count                  = var.enable_elasticsearch == "true" ? 1 : 0
+  name                   = "${var.project_name}-es-jumpbox"
+  description            = "controls access to Kibana"
+  vpc_id                 = aws_vpc.management_vpc.id
+  revoke_rules_on_delete = true
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "443"
+    to_port     = "443"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name             = var.project_name
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
+
+resource "aws_instance" "jumpbox" {
+  count                       = var.enable_jumpbox == "true" ? 1 : 0
+  ami                         = data.aws_ami.amzn.id
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+
+  # referring to the key pair to be used to SSH into box
+  key_name               = "jumpbox-${var.project_name}"
+  subnet_id              = aws_subnet.private[0].id
+  vpc_security_group_ids = [aws_security_group.es_jumpbox[0].id]
+
+  tags = {
+    Name             = "${var.project_name} Management Jumpbox"
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
---
+++ cmd/setup/templates/terraform/management/nginx.tf
@@ -0,0 +1,159 @@
+#======================================================================================================================
+# DNS
+#======================================================================================================================
+resource "aws_route53_record" "nginx" {
+  zone_id = data.terraform_remote_state.dns.outputs.main_zone_id
+  name    = "monitoring.${var.project_name}.${var.hosted_zone}"
+  type    = "A"
+  records = [aws_instance.nginx.public_ip]
+  ttl     = 300
+}
+
+#======================================================================================================================
+# SECURITY GROUP
+#======================================================================================================================
+resource "aws_security_group" "nginx" {
+  name                   = "${var.project_name}-management-nginx"
+  description            = "controls access to nginx"
+  vpc_id                 = aws_vpc.management_vpc.id
+  revoke_rules_on_delete = true
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "22"
+    to_port     = "22"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "80"
+    to_port     = "80"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = "443"
+    to_port     = "443"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.project_name
+  }
+}
+
+#======================================================================================================================
+# INSTANCES
+#======================================================================================================================
+resource "aws_instance" "nginx" {
+  # The connection block tells our provisioner how to
+  # communicate with the resource (instance)
+  connection {
+    # The default username for our AMI
+    user        = "ubuntu"
+    type        = "ssh"
+    private_key = tls_private_key.jumpbox.private_key_pem
+    host        = self.public_ip
+
+    # The connection will use the local SSH agent for authentication.
+  }
+
+  associate_public_ip_address = true
+  instance_type               = "t2.small"
+
+  # Lookup the correct AMI based on the region
+  # we specified 
+  ami = "ami-05c1fa8df71875112"
+
+  # The name of our SSH keypair we created above.
+  key_name = "jumpbox-${var.project_name}"
+
+  # Our Security group to allow HTTP and SSH access
+  vpc_security_group_ids = [aws_security_group.nginx.id]
+
+  # We're going to launch into the same subnet as our ELB. In a production
+  # environment it's more common to have a separate private subnet for
+  # backend instances.
+  subnet_id = aws_subnet.private.0.id
+
+  # Copy the certificate into the dir where nginx conf is expecting it to be
+  provisioner "file" {
+    content     = acme_certificate.certificate.certificate_pem
+    destination = "/tmp/certificate.pem"
+  }
+
+  provisioner "file" {
+    content     = acme_certificate.certificate.issuer_pem
+    destination = "/tmp/issuer.pem"
+  }
+
+  provisioner "file" {
+    source      = "./default.conf"
+    destination = "/tmp/default.conf"
+  }
+
+  provisioner "file" {
+    source      = ".htpasswd"
+    destination = "/tmp/.htpasswd"
+  }
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "remote-exec" {
+    inline = [
+      "host=$(hostname)",
+      "hostIp=$(hostname -I)",
+      "sudo -- sh -c \"echo '$hostIp $host' >> /etc/hosts\"",
+      "sudo apt-get -y update",
+      "sudo apt-get -y install nginx",
+      "sudo mkdir /etc/certs/",
+      "sudo cat /tmp/issuer.pem >> /tmp/certificate.pem",
+      "sudo mv /tmp/certificate.pem /etc/certs",
+      "sudo unlink /etc/nginx/sites-enabled/default",
+      "sudo mv /tmp/.htpasswd /etc/nginx/.htpasswd",
+      "sudo touch /etc/nginx/.htpasswd",
+      "sudo -- sh -c \"echo '${acme_certificate.certificate.private_key_pem}' > /etc/certs/privKey.pem\"",
+      "sudo mv /tmp/default.conf /etc/nginx/sites-enabled/",
+      "sudo sed -i \"s/SERVERNAME/monitoring.${var.project_name}.${var.hosted_zone}/g\" /etc/nginx/sites-enabled/default.conf",
+      "sudo sed -i \"s/KIBANA_URL/${aws_elasticsearch_domain.es[0].endpoint}/g\" /etc/nginx/sites-enabled/default.conf",
+      "RESOLVER_IP=$(grep -o \"[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\\.[0-9]\\{1,3\\}\" /etc/resolv.conf | head -n 1 $1)",
+      "sudo sed -i \"s/RESOLVER_IP/$RESOLVER_IP/g\" /etc/nginx/sites-enabled/default.conf",
+      "sudo service nginx start",
+      "sudo nginx -s reload",
+      "sudo update-rc.d nginx defaults",
+    ]
+  }
+}
+
+#----------------------------------------------------------------------------------------------------------------------
+# LOAD BALANCING
+#----------------------------------------------------------------------------------------------------------------------
+resource "aws_lb_target_group" "nginx" {
+  name        = "${var.project_name}-management-nginx"
+  port        = "80"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.management_vpc.id
+  target_type = "instance"
+
+  tags = {
+    Name             = var.project_name
+    pac-project-name = var.project_name
+    environment      = "management"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "nginx" {
+  target_group_arn = aws_lb_target_group.nginx.arn
+  target_id        = aws_instance.nginx.id
+  port             = 80
+}
