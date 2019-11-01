c3aeac48004375b4d426cae49b72b2b242b4489c
Jeff Diederiks <jdiederiks@psi-it.com>
2019-11-01 14:38:33 -0400

    Added 'Template' ReactJS component

---
+++ cmd/setup/templates/app/src/components/Template/Template.component.tsx
@@ -0,0 +1,35 @@
+// import * as React from 'react';
+// import { hot } from 'react-hot-loader/root';
+// import { connect } from 'react-redux';
+// import './template.scss';
+// import { ApplicationState } from '@app/redux/Reducers/Reducer';
+// import { Dispatch } from "redux";
+
+// export const mapStateToProps = (
+//   state: ApplicationState
+// ): TemplateComponentState => ({});
+
+// export const mapDispatchToProps = (dispatch: Dispatch) =>
+//   ({});
+
+// type TemplateComponentState = {};
+// type ReduxStateToProps = ReturnType<typeof mapStateToProps>;
+// type ReduxDispatchToProps = ReturnType<typeof mapDispatchToProps>;
+// type TemplateProps = TemplateComponentState &
+//   ReduxDispatchToProps &
+//   ReduxStateToProps;
+
+// export class TemplateComponent extends React.Component<TemplateProps> {
+//   render = () => {
+//     return (
+//       <div className="template-component">
+//         TEMPLATE COMPONENT
+//       </div>
+//     );
+//   };
+// }
+
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(hot(TemplateComponent));
---
+++ cmd/setup/templates/app/src/components/Template/index.ts
@@ -0,0 +1,3 @@
+// import Template from './Template.component';
+
+// export { Template };
